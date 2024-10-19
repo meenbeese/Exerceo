231d39543f03e41f4e663e1563374998b6a0f34b
Meenbeese <meenbeese@tutanota.com>
2024-10-18 20:26:30 -0400

    Rewrite SessionSettingsFragment in Kotlin

---
+++ app/src/main/java/com/health/openworkout/gui/session/SessionSettingsFragment.kt
@@ -0,0 +1,74 @@
+package com.health.openworkout.gui.session
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+
+import com.health.openworkout.R
+import com.health.openworkout.core.OpenWorkout
+import com.health.openworkout.core.datatypes.WorkoutSession
+import com.health.openworkout.gui.datatypes.GenericSettingsFragment
+
+class SessionSettingsFragment : GenericSettingsFragment() {
+    private var workoutSession: WorkoutSession? = null
+
+    private var imgView: ImageView? = null
+    private var nameView: TextView? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_sessionsettings, container, false)
+
+        imgView = root.findViewById(R.id.imgView)
+        nameView = root.findViewById(R.id.nameView)
+
+        mode = SessionSettingsFragmentArgs.fromBundle(arguments).mode
+
+        return root
+    }
+
+    override fun getTitle(): String? {
+        return workoutSession?.name
+    }
+
+    override fun loadFromDatabase(mode: SETTING_MODE) {
+        when (mode) {
+            SETTING_MODE.ADD -> workoutSession = WorkoutSession()
+            SETTING_MODE.EDIT -> {
+                val workoutSessionId = SessionSettingsFragmentArgs.fromBundle(arguments).workoutSessionId
+                workoutSession = OpenWorkout.getInstance().getWorkoutSession(workoutSessionId)
+            }
+        }
+
+        if (workoutSession!!.isFinished) {
+            imgView?.setImageResource(R.drawable.ic_session_done)
+        } else {
+            imgView?.setImageResource(R.drawable.ic_session_undone)
+        }
+
+        nameView?.text = workoutSession?.name
+    }
+
+    override fun saveToDatabase(mode: SETTING_MODE): Boolean {
+        workoutSession?.name = nameView?.text.toString()
+
+        when (mode) {
+            SETTING_MODE.ADD -> {
+                val trainingPlanId = SessionSettingsFragmentArgs.fromBundle(arguments).trainingPlanId
+
+                workoutSession?.trainingPlanId = trainingPlanId
+                OpenWorkout.getInstance().insertWorkoutSession(workoutSession)
+            }
+
+            SETTING_MODE.EDIT -> OpenWorkout.getInstance().updateWorkoutSession(workoutSession)
+        }
+
+        return true
+    }
+}
