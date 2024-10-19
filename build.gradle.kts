56203b74134ebd0c2bd97b9e587f1c088b42b8d7
Meenbeese <meenbeese@tutanota.com>
2024-10-18 20:09:45 -0400

    Migrate Gradle buildscript to new format

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
-
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.7.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
+plugins {
+    id("com.android.application") version("8.7.1") apply false
+    id("org.jetbrains.kotlin.android") version("2.0.20") apply false
 }
