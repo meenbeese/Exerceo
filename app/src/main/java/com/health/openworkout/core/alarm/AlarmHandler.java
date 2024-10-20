a44260c519ddb65a41fffb9dc8c8dd4a2538b137
Meenbeese <meenbeese@tutanota.com>
2024-10-20 15:12:57 -0400

    Add a missing pending intent flag

--- app/src/main/java/com/health/openworkout/core/alarm/AlarmHandler.java
+++ app/src/main/java/com/health/openworkout/core/alarm/AlarmHandler.java
@@ -75,7 +75,7 @@ public class AlarmHandler {
         Intent alarmIntent = new Intent(context, ReminderBootReceiver.class);
         alarmIntent.putExtra(INTENT_EXTRA_ALARM, true);
 
-        return PendingIntent.getBroadcast(context, dayOfWeek, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        return PendingIntent.getBroadcast(context, dayOfWeek, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
     }
 
     public void disableAllAlarms(Context context) {
